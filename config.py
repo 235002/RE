ceb264c89d7bc4d087486b36c497b9c248c15c62
235002 <jakub_antoni_pawlowski@wp.pl>
2019-11-19 23:15:21 +0100

    Working with progress

--- config.py
+++ config.py
@@ -1,10 +1,34 @@
 # Users login data
-admin = {
-    "username": "admin",
-    "password": "admin"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjg5NzcyMTEsImlhdCI6MTU2MDg1ODI5N30.xg2gFHZHte6f8oOIgED2Aw9jstwAEl3P0k4ABy2ry74"
+admin_id = "28977211"
+user_id = "28977211"
+
+# Data to insert to DB
+project_data = {
+    "project": {
+            "name": "Performance Testing",
+            "short_description": "Prepare performance tests for web application.",
+            "long_description": "Vulputate odio ut enim blandit volutpat maecenas",
+            "number_of_members": 5,
+            "technology": "Python, Locust",
+            "tags": "WebDev, WebTest",
+            "requirements": "none",
+            "theme_color": "#3f51b5",
+            "verified": 1},
+    "token": token
 }
 
-bjnovak = {
-    "username": "bj",
-    "password": "novak"
+news_data = {
+    "news": {
+            "title": "Need one person to my groupe!",
+            "body": "Hi, I've just created new team, please be welcome to join me!",
+            "user_id": user_id},
+    "token": token
 }
+
+
+# print codes and text function
+def status(response):
+    print("Resposne code:", response.status_code)
+    print("Response plain text: ", response.text)
+
--- locustfile.py
+++ locustfile.py
@@ -1,44 +1,83 @@
-from locust import HttpLocust, TaskSet, between
+from locust import HttpLocust, TaskSet, task
+import json
 import config
 
 
-def login(l, client):
-    l.client.post("/login", {"username": client['username'], "password": client['password']})
-
-
-def logout(l, client):
-    l.client.post("/logout", {"username": client['username'], "password": client['password']})
-
-
-def index(l):
-    l.client.get("/")
-
-
-def profile(l):
-    l.client.get("/profile")
-
-
-class UserBehavior(TaskSet):
-    tasks = {index: 2, profile: 1}
-
-    def __init__(self):
-        self.user = ''
-
-    def set_user(self, user):
-        self.user = user
-
-    def get_user(self):
-        return self.user
+class InnoPointPerformanceTest(TaskSet):
 
     def on_start(self):
-        login(self, self.user)
+        self.login()
 
-    def on_stop(self):
-        logout(self, self.user)
+    def login(self):
+        response = self.client.put("/user", {"token": config.token})
+        config.status(response)
+
+    @task(1)
+    def admin_scenario(self):
+        project_id = self.create_project(config.project_data)
+        self.verify_project(project_id)
+        self.delete_project(project_id)
+
+    @task(1)
+    def user_scenario(self):
+        project_id = self.create_project(config.project_data)
+        self.verify_project(project_id)
+
+        team_id = self.create_team()
+        self.post_news()
+
+        self.join_team(team_id)
+        self.join_project()
+
+        # leave project
+        # leave team
+        # delete team
+        # delete project
+
+    def create_project(self, project_data):
+        response = self.client.post("/projects", json=project_data)
+        config.status(response)
+        response = json.loads(response.text)
+        return response[0]['id']
+
+    def verify_project(self, project_id=1):
+        self.client.put("/projects/verify/{0}".format(project_id), {"token": config.token})
+
+    def create_team(self):
+        team_data = {"team": {
+                "open": 1,
+                "max_number_of_members": 6,
+                "token": config.token
+            }
+        }
+        response = self.client.post("/teams", json=team_data)
+        config.status(response)
+        response = json.loads(response.text)
+        return response[0]['team_id']
+
+    def post_news(self):
+        response = self.client.post("/news", config.news_data)
+        config.status(response)
+
+    def join_team(self, team_id=1):
+        response = self.client.put("/teams/{0}/join".format(team_id), {"token": config.token})
+        config.status(response)
+
+    def join_project(self, project_id=1):
+        response = self.client.put("/project/{0}/apply".format(project_id), {"token": config.token})
+        config.status(response)
+
+    def delete_team(self, team_id):
+        response = self.client.delete("/teams/{0}".format(team_id), {"token": config.token})
+        config.status(response)
+
+    def delete_project(self, project_id):
+        response = self.client.delete("/projects/{0}".format(project_id), {"token": config.token})
+        config.status(response)
 
 
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior()
-    task_set.set_user(config.bjnovak)
-
-    wait_time = between(5.0, 9.0)
+    task_set = InnoPointPerformanceTest
+    host = "http://localhost:3030"
+    min_wait = 2 * 1000
+    max_wait = 6 * 1000
